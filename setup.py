7127286b0ba0114f05b9470c8c940ca26a1e70f2
Sumit <49120301+esumit-github@users.noreply.github.com>
2024-06-04 15:36:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ setuptools.setup(
     description="A small python package",
     long_description=long_description,
     long_description_content="text/markdown",
-    #url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     #url=f"https://{AUTHOR_USER_NAME}:{AUTHOR_PAT}@github.rsym-cicd.blr.rsicdcdns.lab/sumit-mitra/{REPO_NAME}",
-    url=f"http.proxy=http://192.168.50.3:3128 https://{AUTHOR_USER_NAME}:{AUTHOR_PAT}@github.rsym-cicd.blr.rsicdcdns.lab/sumit-mitra/{REPO_NAME}",
+    #url=f"http.proxy=http://192.168.50.3:3128 https://{AUTHOR_USER_NAME}:{AUTHOR_PAT}@github.rsym-cicd.blr.rsicdcdns.lab/sumit-mitra/{REPO_NAME}",
     
     project_urls={
-        #"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-        "Bug Tracker": f"github.rsym-cicd.blr.rsicdcdns.lab/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        #"Bug Tracker": f"github.rsym-cicd.blr.rsicdcdns.lab/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
